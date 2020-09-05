c335040289f2c9855889580497e04f2cdaad570c
Kazuki Sawada <kazuki@6715.jp>
2020-09-05 17:41:32 +0900

    change pma user

--- hq/phpmyadmin/config.inc.php
+++ hq/phpmyadmin/config.inc.php
@@ -7,8 +7,9 @@ $cfg['blowfish_secret'] = 'isucon10-narusejun';
 for ($i = 1; $i <= 3; $i++) {
 	$cfg['Servers'][$i]['auth_type'] = 'config';
 	$cfg['Servers'][$i]['host'] = 'tun_s' . $i;
-	$cfg['Servers'][$i]['user'] = 'isucon';
-	$cfg['Servers'][$i]['password'] = 'isucon';
+	$cfg['Servers'][$i]['user'] = 'root';
+	$cfg['Servers'][$i]['password'] = '';
+	$cfg['Servers'][$i]['AllowNoPassword'] = true;
 }
 
 $cfg['UploadDir'] = '';
