19c2c7932a41dbbd93ee26212c8db7aa7e0ca37d
Kazuki Sawada <kazuki@6715.jp>
2020-09-05 14:32:52 +0900

    pma

---
+++ hq/phpmyadmin/config.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+$cfg['blowfish_secret'] = 'isucon10-narusejun';
+
+for ($i = 1; $i <= 3; $i++) {
+	$cfg['Servers'][$i]['auth_type'] = 'config';
+	$cfg['Servers'][$i]['host'] = 'tun_s' . $i;
+	$cfg['Servers'][$i]['user'] = 'isucon';
+	$cfg['Servers'][$i]['password'] = 'isucon';
+}
+
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
